2e5acdc79fc9a9af6860e41a43df437051f1042b
marishikaw <90078966+marishikaw@users.noreply.github.com>
2022-09-30 21:16:32 +0900

    initial commit

---
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.memorygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView curView = null;
+    private int countPair = 0;
+    final int[] drawable = new int[]{
+            R.drawable.apple,
+            R.drawable.banana,
+            R.drawable.grape,
+            R.drawable.orange,
+            R.drawable.peach,
+            R.drawable.strawberry
+    };
+    int[] pos = {0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5};   // シャッフルしたい
+    int currentPos = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        //インスタンス生成
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(imageAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //1枚目
+                if (currentPos < 0 ) {
+                    currentPos = position;
+                    curView = (ImageView) view;
+                    ((ImageView) view).setImageResource(drawable[pos[position]]);
+                //2枚目
+                } else {
+                    //1枚目と同じ場所をクリックした場合
+                    if (currentPos == position) {
+                        ((ImageView) view).setImageResource(R.drawable.card);
+                    //不一致だった場合
+                    } else if (pos[currentPos] != pos[position]) {
+                        curView.setImageResource(R.drawable.card);
+                        Toast.makeText(MainActivity.this, "Not Match!", Toast.LENGTH_LONG).show();
+                    //一致だった場合
+                    } else {
+                        ((ImageView) view).setImageResource(drawable[pos[position]]);
+                        countPair++;
+                        if (countPair == 6) {
+                            Toast.makeText(MainActivity.this, "You Win!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    currentPos = -1;
+                }
+            }
+        });
+    }
+}
